24674c8a60ac1ae75b9ecabed0b5cf8a688a5786
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-31 15:16:32 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+
+let student: string = 'jolly';
+let age: number = 75;
+let isSmart: boolean = true;
+let students: string[] = ['Joe', 'Biden', 'lorem'];
+let fees: number[] = [373, 54, 33, 34, 234];
+
+const person: { name: string, job: string, age: number } = {
+  name: 'Bill',
+  job: 'Bekar',
+  age: 85
+}
+
+interface Man {
+  name: string,
+  job?: string,
+  age: number
+}
+
+const man: Man = {
+  name: 'amir',
+  age: 83
+}
+
+console.log(student, age, isSmart);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
